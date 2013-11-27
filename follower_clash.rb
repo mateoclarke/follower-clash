5b85b68cd6fd299890b65c6c563560ae6c2b384a
mateoclarke <code@mateoclarke.com>
2013-11-27 11:45:07 -0600

    Added comparison methods to user_compare.rb and script to compare two twitter handles in follower_clash.rb. Upload before attempting any extentions.

--- follower_clash.rb
+++ follower_clash.rb
@@ -2,3 +2,31 @@ require 'rubygems'
 require 'bundler/setup'
 
 require_relative 'lib/user_comparer'
+
+user1 = UserComparer::User.new('mateoclarke')
+user2 = UserComparer::User.new('clam_stew')
+puts "#{user1.username}'s follower count: #{user1.followers}"
+puts "#{user2.username}'s follower count: #{user2.followers}"
+
+comparer = UserComparer::Comparer.new(user1, user2)
+puts "And the user with the most followers is: #{comparer.compare}"
+
+puts "#{user1.username}'s friend count: #{user1.friends}"
+puts "#{user2.username}'s friend count: #{user2.friends}"
+friends = UserComparer::Comparer.new(user1, user2)
+puts "And the user with the most friends is: #{friends.friend_compare}"
+
+puts "#{user1.username}'s tweet count: #{user1.tweets}"
+puts "#{user2.username}'s tweet count: #{user2.tweets}"
+friends = UserComparer::Comparer.new(user1, user2)
+puts "And the user with the most tweets is: #{friends.tweets_compare}"
+
+puts "#{user1.username}'s last tweet: #{user1.last_tweet}"
+puts "#{user2.username}'s last tweet: #{user2.last_tweet}"
+friends = UserComparer::Comparer.new(user1, user2)
+
+puts "#{user1.username}'s twitter handle was created: #{user1.created}"
+puts "#{user2.username}'s twitter handle was created: #{user2.created}"
+friends = UserComparer::Comparer.new(user1, user2)
+puts "The user with the oldest tweeter account is: #{friends.created_compare}"
+
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -8,18 +8,34 @@ module UserComparer
   	attr_reader :username
   	def initialize(username)
   		@username = username
+  		@client = Twitter::REST::Client.new do |config|
+  		  config.consumer_key        = ENV['CONSUMER_KEY']
+  		  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  		  config.access_token         = ENV['OAUTH_TOKEN']
+  		  config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
+  		 end 
   	end
 
   	def followers
-	  	client = Twitter::REST::Client.new do |config|
-			  config.consumer_key        = ENV['CONSUMER_KEY']
-			  config.consumer_secret     = ENV['CONSUMER_SECRET']
-			  config.access_token         = ENV['OAUTH_TOKEN']
-			  config.access_token_secret  = ENV['OAUTH_TOKEN_SECRET']
-	  	end	
-
-  		client.user(@username)followers_count
+			@client.user(@username).followers_count 	
   	end
+
+  	def friends
+			@client.user(@username).friends_count 	
+  	end
+
+  	def tweets
+			@client.user(@username).statuses_count 	
+  	end
+
+  	def last_tweet
+  		@client.user(@username).status.text
+  	end  	
+
+  	def created
+  		@client.user(@username).created_at
+  	end
+
   end
 
   class Comparer
@@ -30,8 +46,55 @@ module UserComparer
 		end
 
 		def compare
+			if (@user1.followers > @user2.followers)
+				return @user1.username
+			elsif (@user2.followers > @user1.followers)
+				return @user2.username
+			else 
+				return "They are equal."
 			end
+		end
+
+		def friend_compare
+			if (@user1.friends > @user2.friends)
+				return @user1.username
+			elsif (@user2.friends > @user1.friends)
+				return @user2.username
+			else 
+				return "They are equal."
+			end
+		end
+
+		def tweets_compare
+			if (@user1.tweets > @user2.tweets)
+				return @user1.username
+			elsif (@user2.tweets > @user1.tweets)
+				return @user2.username
+			else 
+				return "They are equal."
+			end
+		end
+	
+		def last_tweet_compare
+			if (@user1.last_tweet > @user2.last_tweet)
+				return @user1.username
+			elsif (@user2.last_tweet > @user1.last_tweet)
+				return @user2.username
+			else 
+				return "They are equal."
+			end
+		end
+
+		def created_compare
+			if (@user1.created < @user2.created)
+				return "#{@user1.username} joined twitter first"
+			elsif (@user1.created > @user2.created)
+				return "#{@user2.username} joined twitter first"
+			else 
+				return "They joined at the exact same moment."
+			end
+		end
+	end  
 
 
-  end
 end
